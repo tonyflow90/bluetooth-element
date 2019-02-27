38b3f2a355b162d4a9032133d326c2d03cda6d0d
Bohlen <Keno.Bohlen@gmh-gruppe.de>
2019-02-27 15:11:44 +0100

    1

--- bluetooth-element.js
+++ bluetooth-element.js
@@ -1,36 +1,109 @@
 const template = document.createElement('template');
 // template.innerHTML = '<style>:host {display:inline-block;} ::slotted(*){width:inherit;height:inherit;}</style><div><slot/></div>';
 
+const btServices = [
+    'generic_access',
+    'alert_notification',
+    'automation_io',
+    'battery_service',
+    'blood_pressure',
+    'body_composition',
+    'bond_management',
+    'continuous_glucose_monitoring',
+    'current_time',
+    'cycling_power',
+    'cycling_speed_and_cadence',
+    'device_information',
+    'environmental_sensing',
+    'fitness_machine',
+    'generic_attribute',
+    'glucose',
+    'health_thermometer',
+    'heart_rate',
+    'http_proxy',
+    'human_interface_device',
+    'immediate_alert',
+    'indoor_positioning',
+    // 'insulin_delivery',
+    'internet_protocol_support',
+    'link_loss',
+    'location_and_navigation',
+    'mesh_provisioning',
+    'mesh_proxy',
+    'next_dst_change',
+    'object_transfer',
+    'phone_alert_status',
+    'pulse_oximeter',
+    'reconnection_configuration',
+    'reference_time_update',
+    'running_speed_and_cadence',
+    'scan_parameters',
+    'transport_discovery',
+    'tx_power',
+    'user_data',
+    'weight_scale',
+];
 
 class BluetoothElement extends HTMLElement {
 
     static get observedAttributes() {
-        return ['scale'];
+        return ['services','server'];
     }
 
     constructor(evt) {
         super();
-        this.device = undefined;
+        this.services = [];
         this.server = undefined;
+        this.connected = false;
 
         this.attachShadow({ mode: 'open' });
     }
 
     connect() {
-        navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+        let that = this;
+
+        let optionalServices = [];
+        this.services.forEach( service => {
+            let s = service.split(/, ?/)
+            .map(s => s.startsWith('0x') ? parseInt(s) : s)
+            .filter(s => s && BluetoothUUID.getService);
+
+            if(btServices.includes(service)) {
+                optionalServices = [service, ...optionalServices];
+            } else {
+                console.error(`Service '${service}' does not exists!`);
+            }
+        });
+        let acceptAllDevices = false;
+        if(optionalServices.length <= 0)
+            acceptAllDevices = true;
+
+        navigator.bluetooth.requestDevice({
+            acceptAllDevices: acceptAllDevices,
+            optionalServices: btServices
+            // filters: [{services: optionalServices}]
+        })
             .then(device => {
-                // Human-readable name of the device.
-                console.log(device.name);
-                // Attempts to connect to remote GATT Server.
+                device.addEventListener('gattserverdisconnected', this.onDisconnected);
                 return device.gatt.connect();
             })
             .then(server => {
-                console.log(server);
-                let myservices = server.getPrimaryServices();
+                that.connected = server.connected;
+                if (server.connected) {
+                    that.server = server;
+                } else {
+                    that.server = undefined;
+                }
+                return server.getPrimaryServices()
             })
             .catch(error => {
                 console.log(error);
             });
+    }
+
+    onDisconnected(event) {
+        let device = event.target;
+        console.log('Device ' + device.name + ' is disconnected.');
     }
 
     connectedCallback(evt) {
@@ -44,7 +117,8 @@ class BluetoothElement extends HTMLElement {
 
     attributeChangedCallback(attr, oldVal, newVal) {
         switch (attr) {
-            case 'device':
+            case 'services':
+                this.services = newVal.replace(/\s/g, '').split(',');
                 break;
         }
     }
